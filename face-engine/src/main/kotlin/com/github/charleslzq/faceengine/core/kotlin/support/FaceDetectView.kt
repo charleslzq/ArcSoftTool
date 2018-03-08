9ef5080bb30b5709780e1a0d8877d34fda0950a6
Eric Charles <github@ericcharleslzq.com>
2018-03-08 14:05:56 +0800

    switch to fotoapparat for face detection

---
+++ face-engine/src/main/kotlin/com/github/charleslzq/faceengine/core/kotlin/support/FaceDetectView.kt
@@ -0,0 +1,85 @@
+package com.github.charleslzq.faceengine.core.kotlin.support
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.FrameLayout
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.FotoapparatBuilder
+import io.fotoapparat.log.logcat
+import io.fotoapparat.parameter.ScaleType
+import io.fotoapparat.preview.Frame
+import io.fotoapparat.preview.FrameProcessor
+import io.fotoapparat.selector.external
+import io.fotoapparat.selector.firstAvailable
+import io.fotoapparat.selector.front
+import io.fotoapparat.view.CameraView
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Created by charleslzq on 18-3-8.
+ */
+open class FaceDetectView
+@JvmOverloads
+constructor(context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0) :
+    FrameLayout(context, attributeSet, defStyle) {
+    private var _isRunning = AtomicBoolean(false)
+    val isRunning: Boolean
+        get() = _isRunning.get()
+
+    private val cameraView = CameraView(context, attributeSet, defStyle).also { addView(it) }
+    private val frameProcessor = FrameToObservableProcessor()
+    private val fotoapparat by lazy {
+        Fotoapparat.with(context)
+            .apply { setup(this) }
+            .frameProcessor(frameProcessor)
+            .lensPosition(
+                firstAvailable(
+                    front(),
+                    external()
+                )
+            )
+            .previewScaleType(ScaleType.CenterInside)
+            .into(cameraView)
+            .build()
+    }
+
+    open fun setup(fotoapparatBuilder: FotoapparatBuilder) {
+        fotoapparatBuilder.logger(logcat()).cameraErrorCallback {
+            Log.e(TAG, "Error with fotoapparat", it)
+        }
+    }
+
+    fun start() {
+        if (_isRunning.compareAndSet(false, true)) {
+            fotoapparat.start()
+        }
+    }
+
+    fun stop() {
+        if (_isRunning.compareAndSet(true, false)) {
+            fotoapparat.stop()
+        }
+    }
+
+    @JvmOverloads
+    fun onNewFrame(
+        scheduler: Scheduler = AndroidSchedulers.mainThread(),
+        processor: (Frame) -> Unit
+    ) = frameProcessor.publisher.observeOn(scheduler).subscribe(processor)
+
+    class FrameToObservableProcessor : FrameProcessor {
+        val publisher = PublishSubject.create<Frame>()
+
+        override fun process(frame: Frame) {
+            publisher.onNext(frame)
+        }
+    }
+
+    companion object {
+        const val TAG = "FaceDetectView"
+    }
+}
--- sample/src/main/java/com/github/charleslzq/sample/FaceDetectActivity.kt
+++ sample/src/main/java/com/github/charleslzq/sample/FaceDetectActivity.kt
@@ -5,6 +5,10 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Rect
+import android.graphics.YuvImage
 import android.os.Bundle
 import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
@@ -14,6 +18,8 @@ import com.github.charleslzq.arcsofttools.kotlin.Face
 import com.github.charleslzq.arcsofttools.kotlin.Person
 import com.github.charleslzq.faceengine.core.kotlin.FaceEngineService
 import com.github.charleslzq.faceengine.core.kotlin.store.ReadWriteFaceStore
+import com.guo.android_extend.java.ExtByteArrayOutputStream
+import io.fotoapparat.preview.Frame
 import kotlinx.android.synthetic.main.activity_face_detect.*
 
 class FaceDetectActivity : AppCompatActivity() {
@@ -31,33 +37,30 @@ class FaceDetectActivity : AppCompatActivity() {
     }
     private var faceEngineService: FaceEngineService<Person, Face, Float, ReadWriteFaceStore<Person, Face>>? =
         null
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_face_detect)
-        faceDetectCamera.onNewPicture {
-            counter.text = it.first.toString()
-            resultDisplay.setImageBitmap(it.second)
-            val result =
-                faceEngineService?.detect(it.second)?.mapNotNull { faceEngineService!!.search(it) }
-                        ?: emptyList()
-            if (result.isNotEmpty()) {
-                val person = result.maxBy { it.second }!!.first
-                Log.i("test", "match result : $person")
-                setResult(Activity.RESULT_OK, Intent().apply {
-                    putExtra("personName", person.name)
-                })
-                finish()
-            } else {
-                Log.i("test", "match result not found ${it.first}")
-                if (it.first >= CHECK_LIMIT) {
-                    setResult(Activity.RESULT_CANCELED)
+        faceDetectCamera.onNewFrame {
+            val image = convert(it)
+            Log.i(TAG, "on frame with size ${it.size} and rotation ${it.rotation}")
+            val detectResult = faceEngineService?.detect(image)
+            if (detectResult != null) {
+                val result = detectResult.mapNotNull { faceEngineService!!.search(it) }
+                if (result.isNotEmpty()) {
+                    val person = result.maxBy { it.second }!!.first
+                    toast("Match Result ${person.name}, ${++count}")
+                    Log.i("test", "match result : $person")
+                    setResult(Activity.RESULT_OK, Intent().apply {
+                        putExtra("personName", person.name)
+                    })
                     finish()
-                } else if (it.first >= CHECK_LIMIT / 2) {
-                    faceDetectCamera.period(faceDetectCamera.interval.toLong() * 2) {
-                        it.takePicture()
-                    }
+                } else {
+                    toast("No Match Result, ${++count}")
                 }
+            } else {
+                toast("No Face Detected, ${++count}")
             }
         }
         bindService(
@@ -79,10 +82,19 @@ class FaceDetectActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         unbindService(serviceConnection)
+        faceDetectCamera.stop()
         super.onDestroy()
     }
 
+    private fun convert(frame: Frame) =
+        YuvImage(frame.image, ImageFormat.NV21, frame.size.width, frame.size.height, null).run {
+            ExtByteArrayOutputStream().use {
+                compressToJpeg(Rect(0, 0, frame.size.width, frame.size.height), 100, it)
+                BitmapFactory.decodeByteArray(it.byteArray, 0, it.byteArray.size)
+            }
+        }
+
     companion object {
-        private const val CHECK_LIMIT = 8
+        const val TAG = "FaceDetectActivity"
     }
 }
