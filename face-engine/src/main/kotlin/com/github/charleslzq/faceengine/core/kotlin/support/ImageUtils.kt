d88e0d646d6191a633a22e306f61398d85c2a534
Eric Charles <github@ericcharleslzq.com>
2018-03-08 14:59:10 +0800

    move image data convert functionality to core package

---
+++ face-engine/src/main/kotlin/com/github/charleslzq/faceengine/core/kotlin/support/ImageUtils.kt
@@ -0,0 +1,26 @@
+@file:JvmName("ImageUtils")
+
+package com.github.charleslzq.faceengine.core.kotlin.support
+
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Rect
+import android.graphics.YuvImage
+import io.fotoapparat.preview.Frame
+import java.io.ByteArrayOutputStream
+
+/**
+ * Created by charleslzq on 18-3-8.
+ */
+fun convert(frame: Frame) =
+    YuvImage(frame.image, ImageFormat.NV21, frame.size.width, frame.size.height, null).run {
+        TempByteArrayOutputStream().use {
+            compressToJpeg(Rect(0, 0, frame.size.width, frame.size.height), 100, it)
+            BitmapFactory.decodeByteArray(it.data, 0, it.data.size)
+        }
+    }
+
+private class TempByteArrayOutputStream : ByteArrayOutputStream() {
+    val data: ByteArray
+        get() = super.buf
+}
