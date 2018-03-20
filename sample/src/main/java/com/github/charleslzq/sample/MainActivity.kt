f0ca931351ac67740461cd77bb148e1530efa54e
Eric Charles <github@ericcharleslzq.com>
2018-03-20 10:38:56 +0800

    add register functionality

--- sample/src/main/java/com/github/charleslzq/sample/MainActivity.kt
+++ sample/src/main/java/com/github/charleslzq/sample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.github.charleslzq.sample
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
@@ -10,6 +11,11 @@ import android.os.Bundle
 import android.os.IBinder
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import com.github.charleslzq.arcsofttools.kotlin.Face
 import com.github.charleslzq.arcsofttools.kotlin.Person
@@ -19,6 +25,7 @@ import com.github.charleslzq.faceengine.core.FaceEngineService
 import com.github.charleslzq.facestore.websocket.WebSocketCompositeFaceStore
 import io.fotoapparat.preview.Frame
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) =
         Toast.makeText(this, message, duration).show()
@@ -71,16 +78,50 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         when (RequestCodes.fromCode(requestCode)) {
-            RequestCodes.IMAGE_CAMERA -> if (resultCode == Activity.RESULT_OK && data != null) {
-                faceEngineService?.detect(toFrame(data.extras["data"] as Bitmap))?.run {
-                    if (isNotEmpty()) {
-                        val testPersonId = "test"
-                        faceEngineService!!.store.savePerson(Person(testPersonId, "test_name"))
-                        forEach {
-                            faceEngineService!!.store.saveFace(testPersonId, it)
-                        }
-                    }
+            RequestCodes.IMAGE_CAMERA -> if (resultCode == Activity.RESULT_OK && data != null && faceEngineService != null) {
+                var selectedPerson: SimplePerson? = null
+                val dialogLayout = layoutInflater.inflate(R.layout.dialog_register, null)
+                val autoCompleteTexts = dialogLayout.findViewById<AutoCompleteTextView>(R.id.personRegister)
+                autoCompleteTexts.setAdapter(ArrayAdapter<SimplePerson>(
+                        this,
+                        android.R.layout.simple_dropdown_item_1line,
+                        faceEngineService!!.engine.store.getPersonIds()
+                                .mapNotNull { faceEngineService!!.engine.store.getPerson(it) }
+                                .map { SimplePerson.fromPerson(it) }
+                ))
+                autoCompleteTexts.setOnItemClickListener { parent, _, position, _ ->
+                    @Suppress("UNCHECKED_CAST")
+                    selectedPerson = (parent.adapter as ArrayAdapter<SimplePerson>).getItem(position)
                 }
+                autoCompleteTexts.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    }
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {
+                        selectedPerson = null
+                    }
+                })
+                AlertDialog.Builder(this)
+                        .setView(dialogLayout)
+                        .setTitle("Your ID or Name")
+                        .setPositiveButton("OK", { _, _ ->
+                            val personName = selectedPerson?.name ?: autoCompleteTexts.text.toString()
+                            Log.i("test", personName)
+                            faceEngineService!!.detect(toFrame(data.extras["data"] as Bitmap)).run {
+                                if (isNotEmpty()) {
+                                    val testPersonId = selectedPerson?.id ?: UUID.randomUUID().toString()
+                                    faceEngineService!!.store.savePerson(Person(testPersonId, personName))
+                                    forEach {
+                                        faceEngineService!!.store.saveFace(testPersonId, it)
+                                    }
+                                }
+                            }
+                        })
+                        .setNegativeButton("CANCEL", { dialog, _ -> dialog.dismiss() })
+                        .show()
             }
             RequestCodes.FACE_CHECK -> if (resultCode == Activity.RESULT_OK && data != null) {
                 val personName = data.extras["personName"] as String
@@ -100,6 +141,22 @@ class MainActivity : AppCompatActivity() {
         companion object {
             @JvmStatic
             fun fromCode(requestCode: Int) = RequestCodes.values()[requestCode - 1]
+        }
+    }
+
+    data class SimplePerson(
+            val id: String,
+            val name: String
+    ) {
+        override fun toString() = buildString {
+            append(name)
+            append("#")
+            append(id)
+        }
+
+        companion object {
+            @JvmStatic
+            fun fromPerson(person: Person) = SimplePerson(person.id, person.name)
         }
     }
 }
