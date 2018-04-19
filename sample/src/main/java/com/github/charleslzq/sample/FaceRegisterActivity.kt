651cb88192996b2a0edc26501ab42a2a54303f01
Eric Charles <github@ericcharleslzq.com>
2018-04-19 17:01:04 +0800

    remove useless code

--- sample/src/main/java/com/github/charleslzq/sample/FaceRegisterActivity.kt
+++ sample/src/main/java/com/github/charleslzq/sample/FaceRegisterActivity.kt
@@ -1,38 +1,15 @@
 package com.github.charleslzq.sample
 
 import android.app.Activity
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
-import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
-import com.github.charleslzq.arcsofttools.kotlin.ArcSoftFaceEngineService
-import com.github.charleslzq.arcsofttools.kotlin.Face
-import com.github.charleslzq.arcsofttools.kotlin.Person
-import com.github.charleslzq.arcsofttools.kotlin.WebSocketArcSoftEngineService
 import com.github.charleslzq.faceengine.support.faceEngineTaskExecutor
 import com.github.charleslzq.faceengine.support.toBitmap
-import com.github.charleslzq.facestore.websocket.WebSocketCompositeFaceStore
 import kotlinx.android.synthetic.main.activity_face_register.*
 import java.util.concurrent.atomic.AtomicBoolean
 
 class FaceRegisterActivity : AppCompatActivity() {
-    private val serviceConnection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName) {
-            faceEngineService = null
-        }
-
-        override fun onServiceConnected(name: ComponentName, service: IBinder) {
-            @Suppress("UNCHECKED_CAST")
-            faceEngineService =
-                    service as ArcSoftFaceEngineService<WebSocketCompositeFaceStore<Person, Face>>
-        }
-
-    }
-    private var faceEngineService: ArcSoftFaceEngineService<WebSocketCompositeFaceStore<Person, Face>>? =
-            null
     private var requireTakePicture = AtomicBoolean(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,11 +26,6 @@ class FaceRegisterActivity : AppCompatActivity() {
                 finish()
             }
         }
-        bindService(
-                Intent(this, WebSocketArcSoftEngineService::class.java),
-                serviceConnection,
-                Context.BIND_AUTO_CREATE
-        )
     }
 
     override fun onResume() {
@@ -67,7 +39,6 @@ class FaceRegisterActivity : AppCompatActivity() {
     }
 
     override fun onDestroy() {
-        unbindService(serviceConnection)
         faceRegisterCamera.close()
         faceEngineTaskExecutor.cancelTasks()
         super.onDestroy()
