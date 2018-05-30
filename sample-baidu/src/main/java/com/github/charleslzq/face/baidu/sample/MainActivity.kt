5a41064ba18a8e41006e85fcd76ff4a6b41050c6
Eric Charles <github@ericcharleslzq.com>
2018-05-30 10:13:08 +0800

    simple group add button

--- sample-baidu/src/main/java/com/github/charleslzq/face/baidu/sample/MainActivity.kt
+++ sample-baidu/src/main/java/com/github/charleslzq/face/baidu/sample/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.github.charleslzq.face.baidu.sample
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import com.bin.david.form.data.column.Column
 import com.bin.david.form.data.table.PageTableData
 import com.github.charleslzq.face.baidu.BaiduFaceEngineService
 import com.github.charleslzq.face.baidu.BaiduFaceEngineServiceBackground
 import com.github.charleslzq.face.baidu.BaiduUserApi
 import com.github.charleslzq.face.baidu.data.Image
+import com.github.charleslzq.faceengine.support.runOnUI
 import com.github.charleslzq.faceengine.support.toEncodedBytes
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import java.util.*
+
+fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+    runOnUI {
+        Toast.makeText(this, message, duration).show()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,28 +37,6 @@ class MainActivity : AppCompatActivity() {
                 Log.i("Main", "Service connected ${service.url}")
                 baiduServerUrl.post {
                     baiduServerUrl.setText(service.url)
-                }
-                resetButton.setOnClickListener {
-                    refresh(service)
-                }
-                addFab.setOnClickListener {
-                    startActivityForResult(
-                            Intent(MediaStore.ACTION_IMAGE_CAPTURE),
-                            RequestCodes.IMAGE_CAMERA.code
-                    )
-//                    launch(UI) {
-//                        val groupId = UUID.randomUUID().toString().toUpperCase().replace('-', '_')
-//                        service.addGroup(groupId).await()
-//                        refresh(service)
-//                    }
-                }
-                removeFab.setOnClickListener {
-                    launch(UI) {
-                        service.listGroup().await().result?.groupIdList?.lastOrNull()?.let {
-                            service.deleteGroup(it)
-                        }
-                        refresh(service)
-                    }
                 }
             }.build()
     private val columns: List<Column<String>> = TableColumn.values().map { it.getColumnSetting() }
@@ -60,6 +48,43 @@ class MainActivity : AppCompatActivity() {
             config.apply {
                 isShowXSequence = false
                 isShowYSequence = false
+            }
+        }
+        refreshButton.setOnClickListener {
+            refresh()
+        }
+        addGroupButton.setOnClickListener {
+            if (connection.isConnected) {
+                val dialogLayout = layoutInflater.inflate(R.layout.dialog_group_add, null)
+                val input = dialogLayout.findViewById<EditText>(R.id.groupIdText)
+                AlertDialog.Builder(this@MainActivity)
+                        .setView(dialogLayout)
+                        .setTitle("Input the group id")
+                        .setPositiveButton("OK", null)
+                        .setNegativeButton("CANCEL", { dialog, _ -> dialog.dismiss() })
+                        .show()
+                        .apply {
+                            getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                                val groupId = input.text.toString()
+                                if (groupId.length > 128) {
+                                    input.error = "Group Id is too long, should be 128 byte at most"
+                                } else if (!Regex(BAIDU_ID_REGEX).matches(groupId)) {
+                                    input.error = "Illegal format"
+                                } else if (groupId.isNotBlank()) {
+                                    connection.instance!!.let { service ->
+                                        launch(CommonPool) {
+                                            service.addGroup(groupId)
+                                            refresh()
+                                        }
+                                    }
+                                    dismiss()
+                                } else {
+                                    input.error = "Group Id can not be empty"
+                                }
+                            }
+                        }
+            } else {
+                toast("Service not connected")
             }
         }
         bindService(
@@ -92,7 +117,7 @@ class MainActivity : AppCompatActivity() {
                                     userId, userInfo
                             )).await()
                             print(result.toString())
-                            refresh(service)
+                            refresh()
                         }
                     }
                     else -> {
@@ -102,38 +127,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun refresh(service: BaiduFaceEngineService) = launch(UI) {
-        service.setUrlWithCallback(baiduServerUrl.text.toString()) {
-            launch(UI) {
-                val dataList = mutableListOf<TableItem>()
-                try {
-                    Log.i("Main", "try to listGroup from ${service.url}")
-                    val groupIdList = service.listGroup().await().result?.groupIdList
-                    groupIdList?.forEach { groupId ->
-                        val userIdList = service.listUser(groupId).await().result?.userIdList
-                        if (userIdList == null || userIdList.isEmpty()) {
-                            dataList.add(TableItem(groupId))
-                        } else {
-                            userIdList.forEach { userId ->
-                                val faceList = service.listFace(groupId, userId).await().result?.faceList
-                                if (faceList == null || faceList.isEmpty()) {
-                                    dataList.add(TableItem(groupId, userId))
-                                } else {
-                                    faceList.forEach {
-                                        dataList.add(TableItem(groupId, userId, it.faceToken, it.createTime))
+    fun refresh() = launch(UI) {
+        connection.instance?.let { service: BaiduFaceEngineService ->
+            service.setUrlWithCallback(baiduServerUrl.text.toString()) {
+                launch(UI) {
+                    val dataList = mutableListOf<TableItem>()
+                    try {
+                        Log.i("Main", "try to listGroup from ${service.url}")
+                        val groupIdList = service.listGroup().await().result?.groupIdList
+                        groupIdList?.forEach { groupId ->
+                            val userIdList = service.listUser(groupId).await().result?.userIdList
+                            if (userIdList == null || userIdList.isEmpty()) {
+                                dataList.add(TableItem(groupId))
+                            } else {
+                                userIdList.forEach { userId ->
+                                    val faceList = service.listFace(groupId, userId).await().result?.faceList
+                                    if (faceList == null || faceList.isEmpty()) {
+                                        dataList.add(TableItem(groupId, userId))
+                                    } else {
+                                        faceList.forEach {
+                                            dataList.add(TableItem(groupId, userId, it.faceToken, it.createTime))
+                                        }
                                     }
                                 }
                             }
                         }
+                    } catch (throwable: Throwable) {
+                        Log.e("Main", "Error happened", throwable)
                     }
-                } catch (throwable: Throwable) {
-                    Log.e("Main", "Error happened", throwable)
+                    userInfoTable.tableData = PageTableData<TableItem>(
+                            "Registered Faces",
+                            dataList,
+                            columns
+                    )
                 }
-                userInfoTable.tableData = PageTableData<TableItem>(
-                        "Registered Faces",
-                        dataList,
-                        columns
-                )
             }
         }
     }
@@ -169,5 +196,9 @@ class MainActivity : AppCompatActivity() {
             @JvmStatic
             fun fromCode(requestCode: Int) = RequestCodes.values()[requestCode - 1]
         }
+    }
+
+    companion object {
+        const val BAIDU_ID_REGEX = "[a-zA-Z0-9_]+"
     }
 }
